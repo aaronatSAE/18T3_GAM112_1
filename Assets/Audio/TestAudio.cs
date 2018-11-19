fa4cdadf72eaf9c4e503f5fdc380f6ef630f4800
ConnorC2 <44624716+ConnorC2@users.noreply.github.com>
2018-11-19 23:34:29 +1100

    Audio
    Added audio folder with a variety of useful audio clips, testaudio script inside audio folder.

---
+++ Assets/Audio/TestAudio.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestAudio : MonoBehaviour {
+
+    public float audioLevel = 1.0f;
+
+    public AudioClip jump;
+    public AudioClip purchase;
+    public AudioClip collect;
+    
+    AudioSource audioSource;
+
+	// Use this for initialization
+	void Start ()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+	
+	// Update is called once per frame
+    // Use with IF Statements/OnTrigger/OnCollisions
+	void Update ()
+    {
+        audioSource.PlayOneShot(jump, audioLevel);
+
+        audioSource.PlayOneShot(purchase, audioLevel);
+
+        audioSource.PlayOneShot(collect, audioLevel);
+    }
+}
