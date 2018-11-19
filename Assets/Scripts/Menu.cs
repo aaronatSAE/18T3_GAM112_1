5818400cb3916a193c5e142f5dcc4b123b519edb
FalzerBelmonte <44823402+FalzerBelmonte@users.noreply.github.com>
2018-11-19 12:39:28 +1100

    Added Menu

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void GenderSelection()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Options()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+
+}
